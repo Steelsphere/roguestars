6d7472175b7f5f04b42ccaa414d28f8bbcf160c4
Steelsphere <uber005@gmail.com>
2017-07-30 19:26:07 -0500

    int main was updated

--- roguelike/main.cpp
+++ roguelike/main.cpp
@@ -1,6 +1,12 @@
 #include "main.h"
 
 int main() {
+	
+#ifndef _DEBUG
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+	std::cout.rdbuf(NULL);
+#endif
+	
 	Game game = Game();
 	game.init();
 	game.start();
